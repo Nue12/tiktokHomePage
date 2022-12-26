7ff58e74c0b240d0092a6e7a9db015e5a0c28cec
Nue <phonehtetaung1993@gmail.com>
2022-12-26 20:17:38 +0630

    add swipe feature and if you swipe stop playing video

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-const video = document.querySelector("video");
+const video1 = document.getElementById("video1");
+const video2 = document.getElementById("video2");
 const playPauseContainer = document.querySelector(".playPauseContainer");
-const playTag = document.querySelector(".playIcn");
-const pauseTag = document.querySelector(".pauseIcn");
+const videos = document.querySelectorAll("video");
+console.log(videos);
 
-video.addEventListener("click", playToggle);
+video1.addEventListener("click", () => playToggle(video1));
+video2.addEventListener("click", () => playToggle(video2));
 
-function playToggle() {
+function playToggle(video) {
   video.paused ? video.play() : video.pause();
 }
 
-video.addEventListener("play", () => {
+video1.addEventListener("play", () => {
   playPauseContainer.classList.remove("paused");
   playPauseContainer.style.opacity = "0";
 });
 
-video.addEventListener("pause", () => {
+video1.addEventListener("pause", () => {
   playPauseContainer.style.opacity = "1";
   playPauseContainer.classList.add("paused");
+});
+
+video2.addEventListener("play", () => {
+  playPauseContainer.classList.remove("paused");
+  playPauseContainer.style.opacity = "0";
+});
+
+video2.addEventListener("pause", () => {
+  playPauseContainer.style.opacity = "1";
+  playPauseContainer.classList.add("paused");
+});
+
+const swiper = new Swiper(".swiper", {
+  direction: "vertical",
+  mousewheel: true,
+});
+
+swiper.on("slideChange", function () {
+  videos.forEach((video) => {
+    video.played && video.pause();
+  });
 });
