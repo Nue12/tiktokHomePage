d613aba212980330a1249c6ca8c753bce4667a51
Nue <phonehtetaung1993@gmail.com>
2022-12-28 11:30:21 +0630

    fix the play pause error

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 const video1 = document.getElementById("video1");
 const video2 = document.getElementById("video2");
-const playPauseContainer = document.querySelector(".playPauseContainer");
+const playPauseContainer1 = document.querySelector(".playPause1");
+const playPauseContainer2 = document.querySelector(".playPause2");
 const videos = document.querySelectorAll("video");
-console.log(videos);
+
+const parent1 = playPauseContainer1.parentNode;
+console.log(parent1.lastElementChild);
 
 video1.addEventListener("click", () => playToggle(video1));
+playPauseContainer1.addEventListener("click", () => playToggle(video1));
 video2.addEventListener("click", () => playToggle(video2));
+playPauseContainer2.addEventListener("click", () => playToggle(video2));
 
 function playToggle(video) {
   video.paused ? video.play() : video.pause();
 }
 
 video1.addEventListener("play", () => {
-  playPauseContainer.classList.remove("paused");
-  playPauseContainer.style.opacity = "0";
+  playPauseContainer1.classList.remove("paused");
+  playPauseContainer1.style.opacity = "0";
 });
 
 video1.addEventListener("pause", () => {
-  playPauseContainer.style.opacity = "1";
-  playPauseContainer.classList.add("paused");
+  playPauseContainer1.style.opacity = "1";
+  playPauseContainer1.classList.add("paused");
 });
 
 video2.addEventListener("play", () => {
-  playPauseContainer.classList.remove("paused");
-  playPauseContainer.style.opacity = "0";
+  playPauseContainer2.classList.remove("paused");
+  playPauseContainer2.style.opacity = "0";
 });
 
 video2.addEventListener("pause", () => {
-  playPauseContainer.style.opacity = "1";
-  playPauseContainer.classList.add("paused");
+  playPauseContainer2.style.opacity = "1";
+  playPauseContainer2.classList.add("paused");
 });
 
 // swiper initializing
